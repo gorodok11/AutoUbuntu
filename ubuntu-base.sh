8f9e2709f66a17c4199a204a2ea7257d6c71a423
gorodok11 <gorodok11@gmail.com>
2013-04-01 14:39:42 +0400

    Create ubuntu-base.sh

---
+++ ubuntu-base.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Установка базовой системы с автоматической разметкой
+# После перезагрузки заходим по Администатором системы
+# Обновление базовой системы
+sudo su
+sudo apt-get update && apt-get upgrade
+# Установка программ для легкой работы в консоли
+sudo apt-get -y install htop mc aptitude
+# Установка OpenSSH-server
+sudo apt-get -y install openssh-server && sudo /etc/init.d/ssh start
+# Установка LAMP server
+sudo tasksel install lamp-server
+# Установка phpMyAdmin
+sudo apt-get -y install phpmyadmin
+# Для входа в phpMyAdmin используйте адрес http://server_IP/phpmyadmin
+# Установка WebMin
+sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl libdigest-md5-perl
+echo '# Webmin' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://download.webmin.com/download/repository sarge contrib' | sudo tee -a /etc/apt/sources.list
+cd /tmp
+wget http://www.webmin.com/jcameron-key.asc
+sudo apt-key add jcameron-key.asc
+rm jcameron-key.asc
+sudo apt-get install webmin
+sudo apt-get update && apt-get -y install webmin
+# Для входа в WebMin используйте адрес https://server_IP:10000
+# Установка Git
+sudo apt-get -y install git-core
