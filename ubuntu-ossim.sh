35ee524dbe3c6997f0c579ce60a60d38fb9874ec
gorodok11 <gorodok11@gmail.com>
2013-04-01 17:49:41 +0400

    Update ubuntu-ossim.sh

--- ubuntu-ossim.sh
+++ ubuntu-ossim.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt-get -y install glib2.0 libxml2 glib-networking rrdtool php5 libgda-5.0
+sudo apt-get -y install glib2.0 libxml2 glib-networking rrdtool php5 libgda-5.0 
+sudo apt-get -y install phpgacl
