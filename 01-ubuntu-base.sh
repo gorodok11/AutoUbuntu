1b6caf602569b6ced3ccf973e54da3be24ec0d70
gorodok11 <gorodok11@gmail.com>
2013-04-02 14:18:52 +0400

    Update 01-ubuntu-base.sh

--- 01-ubuntu-base.sh
+++ 01-ubuntu-base.sh
@@ -4,8 +4,16 @@
 # Обновление базовой системы
 sudo su
 sudo apt-get update && apt-get upgrade
+
 # Установка программ для легкой работы в консоли
 sudo apt-get -y install htop mc aptitude zip unzip
+
 # Установка OpenSSH-server
-sudo apt-get -y install openssh-server && sudo /etc/init.d/ssh start
+sudo apt-get -y install openssh-server 
+
+# Просмотр настроек, убрав пустые строки и комментарии:
+# egrep -v '^#' /etc/ssh/sshd_config | egrep -v '^$'
+
+#echo $(date) $SSH_CONNECTION $USER $SSH_TTY >> /var/log/sshd_connect
+sudo /etc/init.d/ssh start
 
