6dbf25dd334dc5976da97d128eaba9ffb1b89f7d
gorodok11 <gorodok11@gmail.com>
2013-04-02 13:14:01 +0400

    Create zz-ubuntu-misc.sh

---
+++ zz-ubuntu-misc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Активируем репозиторий Ubuntu extras
+#Перед импортом ключа убедитесь что порт 11371 не заблокирован
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3E5C1192 
