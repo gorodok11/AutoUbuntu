e0eff96484b8b97426e7ae20963b30da3420c58d
gorodok11 <gorodok11@gmail.com>
2013-04-02 12:57:40 +0400

    Update 01-ubuntu-base.sh

--- 01-ubuntu-base.sh
+++ 01-ubuntu-base.sh
@@ -8,16 +8,6 @@ sudo apt-get update && apt-get upgrade
 sudo apt-get -y install htop mc aptitude zip unzip
 # Установка OpenSSH-server
 sudo apt-get -y install openssh-server && sudo /etc/init.d/ssh start
-# Установка WebMin
-sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl libdigest-md5-perl
-echo '# Webmin' | sudo tee -a /etc/apt/sources.list
-echo 'deb http://download.webmin.com/download/repository sarge contrib' | sudo tee -a /etc/apt/sources.list
-cd /tmp
-wget http://www.webmin.com/jcameron-key.asc
-sudo apt-key add jcameron-key.asc
-rm jcameron-key.asc
-sudo apt-get install webmin
-sudo apt-get update && apt-get -y install webmin
-# Для входа в WebMin используйте адрес https://server_IP:10000
+
 # Установка Git
 sudo apt-get -y install git-core
