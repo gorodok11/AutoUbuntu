bc3a5b779e8b2d123cda22cd6c6f8b14d17503b7
gorodok11 <gorodok11@gmail.com>
2013-04-02 12:56:56 +0400

    Create 02-ubuntu-lamp.sh

---
+++ 02-ubuntu-lamp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Установка LAMP server
+sudo tasksel install lamp-server
+# Установка phpMyAdmin
+sudo apt-get -y install phpmyadmin
+# Для входа в phpMyAdmin используйте адрес http://server_IP/phpmyadmin
