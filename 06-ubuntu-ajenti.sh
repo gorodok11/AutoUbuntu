0900b61619a5d8060ea14594c8e8fbff84e3410b
gorodok11 <gorodok11@gmail.com>
2013-04-05 16:11:46 +0400

    Create 06-ubuntu-ajenti.sh

---
+++ 06-ubuntu-ajenti.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tput setaf 2
+echo "Установка Ajenti"
+tput sgr0
+
+echo '# Ajenti' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://repo.ajenti.org/debian main main' | sudo tee -a /etc/apt/sources.list
+
+cd /tmp
+wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
+apt-get update && apt-get -y install ajenti
+
+# Defaults:
+# port:8000
+# User = admin
+# Password = admin
+ 
