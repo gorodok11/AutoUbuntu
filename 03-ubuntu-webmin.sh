d6e3d5203d2e4dd46942b56c17a3bcf2fee7d9b4
gorodok11 <gorodok11@gmail.com>
2013-04-05 11:53:28 +0400

    Update 03-ubuntu-webmin.sh

--- 03-ubuntu-webmin.sh
+++ 03-ubuntu-webmin.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# Установка WebMin
-sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl libdigest-md5-perl
+tput setaf 2
+echo "Установка WebMin"
+tput sgr0
+apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl libdigest-md5-perl
 echo '# Webmin' | sudo tee -a /etc/apt/sources.list
 echo 'deb http://download.webmin.com/download/repository sarge contrib' | sudo tee -a /etc/apt/sources.list
 cd /tmp
 wget http://www.webmin.com/jcameron-key.asc
-sudo apt-key add jcameron-key.asc
+apt-key add jcameron-key.asc
 rm jcameron-key.asc
-sudo apt-get install webmin
-sudo apt-get update && apt-get -y install webmin
+apt-get install webmin
+apt-get update && apt-get -y install webmin
 # Для входа в WebMin используйте адрес https://server_IP:10000
