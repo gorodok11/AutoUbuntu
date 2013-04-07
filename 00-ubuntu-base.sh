ac253f660d68dd3683e28c07f0cb0f0cef0b0a6e
gorodok11 <gorodok11@gmail.com>
2013-04-07 11:14:01 +0400

    Update 00-ubuntu-base.sh

--- 00-ubuntu-base.sh
+++ 00-ubuntu-base.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Установка базового ПО.
 
-# Связка функций из основного файла
-source 00-functions.sh
-
 # Установка программ для легкой работы в консоли
 apt-get -y install htop mc aptitude zip unzip chkconfig
 
